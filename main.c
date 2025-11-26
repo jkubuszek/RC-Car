98f0e256a5510c4d8c0574a85c6cd9e5b5af2c2e
jkubusze <jkubuszek@student.agh.edu.pl>
2025-11-26 15:48:08 +0100

    engine directions 2

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "MKL05Z4.h"
 #include "InitPWM.c"
+#include "InitUART.c"
 
 struct r_engine{
     in1 = 0;
@@ -8,17 +9,45 @@ struct r_engine{
 };
     
 struct l_engine{
-    in1 = 0;
-    in2 = 0;
+    in3 = 0;
+    in4 = 0;
     en = 0;  
 };
 
-void rdirr(int dirr){
+void forward(){
+    r_engine.in1 = 1;
+    r_engine.in2 = 0;
+    l_engine.in3 = 1;
+    l_engine.in4 = 0;
 
+    r_engine.en = 1;
+    l_engine.en = 1;
+}
+
+void backward(){
+    r_engine.in1 = 0;
+    r_engine.in2 = 1;
+    l_engine.in3 = 0;
+    l_engine.in4 = 1;
+
+    r_engine.en = 1;
+    l_engine.en = 1;
+}
+
+void stop(){
+    r_engine.in1 = 0;
+    r_engine.in2 = 0;
+    l_engine.in3 = 0;
+    l_engine.in4 = 0;
+
+    r_engine.en = 0;
+    l_engine.en = 0;
 }
 
 int main(){
     InitPWM();
+    InitUART();
+    //InitDrive(); in progress
     
     return 0;
 }
